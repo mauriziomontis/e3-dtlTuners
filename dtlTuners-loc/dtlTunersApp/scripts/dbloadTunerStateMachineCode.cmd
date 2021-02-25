8ad27cb8e6dbb9d1d75b58d14163075300efb038
mauriziomontis <maurizio.montis@outlook.com>
2021-02-25 15:25:49 +0100

    update tuners state machine state code management

--- dtlTuners-loc/dtlTunersApp/scripts/dbloadTunerStateMachineCode.cmd
+++ dtlTuners-loc/dtlTunersApp/scripts/dbloadTunerStateMachineCode.cmd
@@ -1,15 +1,17 @@
 #==============================================================================
 # dbloadTunerStateMachineCode.cmd
-#- Arguments: 
+#- Arguments: SECTION_NAME
 
 #-d /**
 #-d   \brief Load Tuners Description Database
 #-d   \details Load Tuners description records which indicate information in the GUI.
 #-d   \author Maurizio Montis (INFN-LNL)
+#-d   \file
+#-d   \param SECTION_NAME Section name, i.e. DTL-010
 #-d   \note Example call:
 #-d   \code
-#-d    ${SCRIPTEXEC} "${dtlTuner_DIR}dbloadTunerStateMachineCode.cmd"
+#-d    ${SCRIPTEXEC} "${dtlTuner_DIR}dbloadTunerStateMachineCode.cmd", "SECTION_NAME=DTL-010"
 #-d   \endcode
 #-d */
 
-dbLoadRecords(dtlTunerStateMachine.db)
+dbLoadRecords(dtlTunerStateMachine.db,"PREFIX=${SECTION_NAME}")
